2f59698f7f0b9817bd9a9c72d6fba4da8cf82f1e
Kaushan M.K.A <anushangamunasinghe@gmail.com>
2024-06-24 22:58:48 +0530

    setup the frontend

--- Backend/app.js
+++ Backend/app.js
@@ -5,14 +5,14 @@ const User = require('./model/Model');
 const userRoutes = require('./routes/Routes'); // Updated import
 
 const app = express();
-const port = 5000;
+const port = 5001; // Changed port number
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect("", {
+mongoose.connect("mongodb+srv://Anushanga:Anushanga2001@cluster0.4d2u3vi.mongodb.net", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -26,5 +26,3 @@ app.use('/users', userRoutes); // Use the correct route file
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
-
